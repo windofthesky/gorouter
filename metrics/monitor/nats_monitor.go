dfa8adbf9f6da4a6166298687141e64f40cc94ba
Aaron Hurley <ahurley@pivotal.io>
2017-11-22 12:29:45 -0800

    WIP [#152164959]
    Signed-off-by: Nick Mahoney <nmahoney@pivotal.io>

--- metrics/monitor/nats_monitor.go
+++ metrics/monitor/nats_monitor.go
@@ -1,21 +1,30 @@
 package monitor
 
-import "time"
+import (
+	"os"
+	"time"
+
+	"github.com/cloudfoundry/dropsonde/metrics"
+)
 
 type NATSMonitor struct {
-	Sender   Metrics.MetricSender
+	Sender   metrics.MetricSender
 	TickChan chan time.Time
 }
 
-func (n *NATSMonitor) Run() error {
+func (n *NATSMonitor) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
+	close(ready)
 	for {
 		select {
-		case n.TickChan:
+		case <-n.TickChan:
 			//get the valude from nats subscriber.Pending()
 			// send the valude using dropsonde value chainer Send()
-			//n.Sender.Value()
-
+			pending := 1
+			chainer := n.Sender.Value("buffered_messages", float64(pending), "")
+			err := chainer.Send()
+			if err != nil {
+				panic("panic")
+			}
 		}
-
 	}
 }
--- metrics/monitor/nats_monitor_test.go
+++ metrics/monitor/nats_monitor_test.go
@@ -1,33 +1,78 @@
 package monitor_test
 
 import (
+	"errors"
 	"time"
 
 	"code.cloudfoundry.org/gorouter/metrics/fakes"
 	"code.cloudfoundry.org/gorouter/metrics/monitor"
+	"github.com/cloudfoundry/dropsonde/metric_sender"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/tedsuo/ifrit"
 )
+
+type FakeValueChainer struct {
+	SendCall struct {
+		CallCount int
+		Returns   struct {
+			Error error
+		}
+	}
+}
+
+func (v *FakeValueChainer) Send() error {
+	v.SendCall.CallCount++
+	return v.SendCall.Returns.Error
+}
+
+func (v *FakeValueChainer) SetTag(key, value string) metric_sender.ValueChainer {
+	return nil
+}
 
 var _ = Describe("NATSMonitor", func() {
 	var (
-		sender *fakes.MetricSender
+		sender           *fakes.MetricSender
+		ch               chan time.Time
+		fakeValueChainer *FakeValueChainer
+		natsMonitor      *monitor.NATSMonitor
 	)
+	BeforeEach(func() {
+		ch = make(chan time.Time)
 
-	It("sends a metric on a time interval", func() {
-		ch := make(chan time.Time)
+		sender = new(fakes.MetricSender)
+		fakeValueChainer = &FakeValueChainer{}
 
-		sender := new(fakes.MetricSender)
-
-		natsMonitor := monitor.NATSMonitor{
+		sender.ValueStub = func(metric string, value float64, unit string) metric_sender.ValueChainer {
+			return fakeValueChainer
+		}
+		natsMonitor = &monitor.NATSMonitor{
 			Sender:   sender,
 			TickChan: ch,
 		}
+	})
 
-		Expect(natsMonitor.Run()).To(Succeed())
-		Expect(sender.ValueCallCount()).To(Equal(1))
-		name, _, unit := sender.ValueArgsForCall()
+	FIt("sends a metric on a time interval", func() {
+		process := ifrit.Invoke(natsMonitor)
+		Eventually(process.Ready()).Should(BeClosed())
+
+		ch <- time.Time{}
+		ch <- time.Time{}
+
+		Eventually(sender.ValueCallCount()).Should(BeNumerically(">", 0))
+		name, _, unit := sender.ValueArgsForCall(0)
 		Expect(name).To(Equal("buffered_messages"))
 		Expect(unit).To(Equal(""))
+		Expect(fakeValueChainer.SendCall.CallCount).To(BeNumerically(">", 0))
+	})
+
+	It("should log an error when Send fails", func() {
+		fakeValueChainer.SendCall.Returns.Error = errors.New("send failed")
+		process := ifrit.Invoke(natsMonitor)
+		Eventually(process.Ready()).Should(BeClosed())
+		ch <- time.Time{}
+		ch <- time.Time{}
+
+		Eventually(sender.ValueCallCount()).Should(BeNumerically(">", 0))
 	})
 })
